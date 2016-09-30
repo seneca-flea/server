f7fc848c69e449451249a7fd2778c602bc72dfac
Miguel <mige@airmail.cc>
2016-09-29 21:43:05 -0400

    Add DataContext and model classes

---
+++ SenecaFleaServer/Models/ModelClasses/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SenecaFleaServer.Models.ModelClasses
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        [StringLength(25)]
+        public string Name { get; set; }
+    }
+
+    public class CourseItem
+    {
+    }
+}
---
+++ SenecaFleaServer/Models/ModelClasses/ItemStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SenecaFleaServer.Models.ModelClasses
+{
+    public class ItemStatus
+    {
+        public int StatusId { get; set; }
+
+        [StringLength(35)]
+        public string StatusName { get; set; }
+    }
+}
